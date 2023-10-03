2ae42bc6b96f9d4651cce251d70af90adbd41990
Naoh <27915369+N40h@users.noreply.github.com>
2023-10-03 16:59:53 +0200

    Created the index and sign in page from static site to react app component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import Features from "../components/Features/Features";
+
+export default function Home() {
+    return (
+        <main>
+            <Hero />
+            <Features />
+        </main>
+    )
+}
